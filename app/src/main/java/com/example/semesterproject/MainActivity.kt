488f1bd3b8fec0d171f9404079117334c93b701d
JKaulback <jtkaulback@gmail.com>
2025-10-31 14:14:41 -0300

    Lab 5: Part 2 - Created PredictionActivity.kt and corresponding activity_prediction.xml layout

--- app/src/main/java/com/example/semesterproject/MainActivity.kt
+++ app/src/main/java/com/example/semesterproject/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.semesterproject
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
@@ -37,7 +35,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         // Declare the custom Toolbar
