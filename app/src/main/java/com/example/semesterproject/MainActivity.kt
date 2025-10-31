64db2672048d81413f54071a93c8dba263260409
JKaulback <jtkaulback@gmail.com>
2025-10-31 12:31:55 -0300

    Lab 5: Part 1 (completed) - Added Intent support for PredictionActivity. Handles rendering

--- app/src/main/java/com/example/semesterproject/MainActivity.kt
+++ app/src/main/java/com/example/semesterproject/MainActivity.kt
@@ -1,38 +1,55 @@
 package com.example.semesterproject
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
     private var userName: String? = ""
+    private var predictionCount: Int = 0
+    private var predictionStringBuilder = StringBuilder()
 
+    // UI widgets
     private lateinit var userNameInputView: EditText
     private lateinit var predictButton: Button
+    private lateinit var predictionHistoryTextView: TextView
+
+    private val predictWeatherLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        // If the weather was successfully predicted
+        if (result.resultCode == RESULT_OK) {
+            // Grab the prediction string
+            result.data?.let { resultIntent ->
+                // Update the historical predictions
+                updateHistoricalPredictions(resultIntent)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Initialize and configure the weather prediction button
-        predictButton =findViewById(R.id.activity_main_predict_button)
+        // Declare the custom Toolbar
+        setSupportActionBar(findViewById(R.id.activity_main_toolbar))
+
+        userNameInputView = findViewById(R.id.activity_main_name_input_edit_text)
+        predictButton = findViewById(R.id.activity_main_predict_button)
+        predictionHistoryTextView = findViewById(R.id.activity_main_predictions_textView)
+
         predictButton.setOnClickListener {
             handlePredictButtonClick()
         }
-
-        // Initialize the EditText element for the user name
-        userNameInputView = findViewById(R.id.activity_main_name_input_edit_text)
-
     }
 
     override fun onStop() {
@@ -48,6 +65,38 @@ class MainActivity : AppCompatActivity() {
 
     private fun handlePredictButtonClick() {
         readName()
+
+        predictWeatherLauncher.launch(
+            PredictionActivity.newIntent(
+                this@MainActivity,
+                userName
+            )
+        )
+    }
+
+    private fun updateHistoricalPredictions(intent: Intent) {
+        val predictionString = intent.getStringExtra(PredictionActivity.PREDICTION_INTENT_EXTRA)
+        // Append the new count to the historical prediction string
+        predictionCount++
+        predictionStringBuilder.clear()
+        updatePredictionCountString()
+
+        // Append prediction text to historical prediction string
+        predictionStringBuilder.append(predictionString)
+
+        // Update the rendered historical prediction
+        predictionHistoryTextView.text = getString(
+            R.string.placeholder,
+            predictionHistoryTextView.text.toString(),
+            predictionStringBuilder.toString()
+        )
+
+    }
+
+    private fun updatePredictionCountString() {
+        predictionStringBuilder.append("#")
+        predictionStringBuilder.append(predictionCount)
+        predictionStringBuilder.append(". ")
     }
 
     companion object {
