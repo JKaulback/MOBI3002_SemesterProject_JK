b6606ce63581ff4ad353bcb0088de69a6d660397
JKaulback <jtkaulback@gmail.com>
2025-11-10 18:13:44 -0400

    Lab 6: Part 5 (completed) - Parsed forecastResponse object and removed prediction history from MainActivity.kt

--- app/src/main/java/com/example/semesterproject/MainActivity.kt
+++ app/src/main/java/com/example/semesterproject/MainActivity.kt
@@ -1,35 +1,26 @@
 package com.example.semesterproject
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private var userName: String? = ""
-    private var predictionCount: Int = 0
-    private var predictionStringBuilder = StringBuilder()
 
     // UI widgets
     private lateinit var userNameInputView: EditText
     private lateinit var predictButton: Button
-    private lateinit var predictionHistoryTextView: TextView
 
     private val predictWeatherLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
         // If the weather was successfully predicted
         if (result.resultCode == RESULT_OK) {
-            // Grab the prediction string
-            result.data?.let { resultIntent ->
-                // Update the historical predictions
-                updateHistoricalPredictions(resultIntent)
-            }
+            Log.d(TAG, "Prediction Activity launched")
         }
     }
 
@@ -43,7 +34,6 @@ class MainActivity : AppCompatActivity() {
 
             userNameInputView = findViewById(R.id.activity_main_name_input_edit_text)
             predictButton = findViewById(R.id.activity_main_predict_button)
-            predictionHistoryTextView = findViewById(R.id.activity_main_predictions_textView)
 
             predictButton.setOnClickListener {
                 handlePredictButtonClick()
@@ -75,31 +65,6 @@ class MainActivity : AppCompatActivity() {
                 userName
             )
         )
-    }
-
-    private fun updateHistoricalPredictions(intent: Intent) {
-        val predictionString = intent.getStringExtra(PredictionActivity.PREDICTION_INTENT_EXTRA)
-        // Append the new count to the historical prediction string
-        predictionCount++
-        predictionStringBuilder.clear()
-        updatePredictionCountString()
-
-        // Append prediction text to historical prediction string
-        predictionStringBuilder.append(predictionString)
-
-        // Update the rendered historical prediction
-        predictionHistoryTextView.text = getString(
-            R.string.placeholder,
-            predictionHistoryTextView.text.toString(),
-            predictionStringBuilder.toString()
-        )
-
-    }
-
-    private fun updatePredictionCountString() {
-        predictionStringBuilder.append("#")
-        predictionStringBuilder.append(predictionCount)
-        predictionStringBuilder.append(". ")
     }
 
     companion object {
--- app/src/main/java/com/example/semesterproject/PredictionActivity.kt
+++ app/src/main/java/com/example/semesterproject/PredictionActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -20,8 +22,15 @@ class PredictionActivity : AppCompatActivity() {
 
     // UI widgets
     private lateinit var iPredictTextView: TextView
-    private lateinit var predictionTextView: TextView
+    private lateinit var areaTextView: TextView
+    private lateinit var latLonTextView: TextView
+    private lateinit var localTimeTextView: TextView
+    private lateinit var tempTimeTextView: TextView
+    private lateinit var tempTextView: TextView
+    private lateinit var windSpeedTextView: TextView
+    private lateinit var windDirectionTextView: TextView
     private lateinit var finishedButton: Button
+    private lateinit var progressBar: ProgressBar
 
     private var iPredictStringBuilder = StringBuilder()
 
@@ -42,15 +51,22 @@ class PredictionActivity : AppCompatActivity() {
 
         Log.d(TAG, "PredictionActivity setting Views")
         iPredictTextView = findViewById(R.id.prediction_activity_i_predict_textView)
-        predictionTextView = findViewById(R.id.prediction_activity_prediction_textView)
+        areaTextView = findViewById(R.id.prediction_activity_location_area_textView)
+        latLonTextView = findViewById(R.id.prediction_activity_location_lat_lon_textView)
+        localTimeTextView = findViewById(R.id.prediction_activity_location_local_time_textView)
+        tempTimeTextView = findViewById(R.id.prediction_activity_current_temp_time_textView)
+        tempTextView = findViewById(R.id.prediction_activity_current_temp_textView)
+        windSpeedTextView = findViewById(R.id.prediction_activity_current_wind_speed_textView)
+        windDirectionTextView = findViewById(R.id.prediction_activity_current_wind_dir_textView)
         finishedButton = findViewById(R.id.prediction_activity_finish_button)
+        progressBar = findViewById(R.id.prediction_activity_progressBar)
 
         processIntent()
         // Return a prediction to the calling activity and finish
         Log.d(TAG, "PredictionActivity setting finishedButton onClickListener")
         finishedButton.setOnClickListener {
             val resultIntent = Intent().apply {
-                putExtra(PREDICTION_INTENT_EXTRA, predictionTextView.text)
+
             }
             setResult(RESULT_OK, resultIntent)
             finish()
@@ -75,6 +91,7 @@ class PredictionActivity : AppCompatActivity() {
     }
 
     private fun updateForecast() {
+        progressBar.visibility = View.VISIBLE
         Log.d(TAG, "PredictionActivity launching coroutine")
         coroutineScope.launch {
             Log.d(TAG, "Coroutine launched")
@@ -83,7 +100,44 @@ class PredictionActivity : AppCompatActivity() {
                 val forecastResponse = WeatherRetrofitApi.getForecast("Halifax", 1)
                 Log.d(TAG, "Response recieved... Attempting to update predictionTextView")
                 runOnUiThread {
-                    predictionTextView.text = forecastResponse.toString()
+                    areaTextView.text = getString(
+                        R.string.area_placeholder,
+                        forecastResponse.location.name,
+                        forecastResponse.location.region,
+                        forecastResponse.location.country)
+
+                    latLonTextView.text = getString(
+                        R.string.lat_lon_placeholder,
+                        forecastResponse.location.lat,
+                        forecastResponse.location.lon
+                    )
+
+                    localTimeTextView.text = getString(
+                        R.string.local_time_placeholder,
+                        forecastResponse.location.localtime
+                    )
+
+                    tempTimeTextView.text = getString(
+                        R.string.temp_time_placeholder,
+                        forecastResponse.current.last_updated
+                    )
+
+                    tempTextView.text = getString(
+                        R.string.temp_placeholder,
+                        forecastResponse.current.temp_c,
+                        forecastResponse.current.temp_f
+                    )
+
+                    windSpeedTextView.text = getString(
+                        R.string.wind_speed_placeholder,
+                        forecastResponse.current.wind_kph,
+                        forecastResponse.current.wind_mph
+                    )
+
+                    windDirectionTextView.text = getString(
+                        R.string.wind_dir_placeholder,
+                        forecastResponse.current.wind_dir
+                    )
                 }
 
             } catch(e: Exception) {
@@ -95,6 +149,8 @@ class PredictionActivity : AppCompatActivity() {
                     toast.setText(getString(R.string.weather_fetch_error))
                     toast.show()
                 }
+            } finally {
+                runOnUiThread { progressBar.visibility = View.INVISIBLE }
             }
         }
     }
@@ -109,9 +165,6 @@ class PredictionActivity : AppCompatActivity() {
         private const val EXTRA_NAME = "com.example.semesterproject.predictionactivity.name"
 
         private const val TAG = "com.example.semesterproject.predictionactivity"
-
-        // Public key to package up new prediction to be passed to previous activity
-        const val PREDICTION_INTENT_EXTRA = "com.example.semesterproject.predictionactivity.prediction"
 
         // A utility function called by any activity to start this one
         fun newIntent(context: Context, userName: String?) : Intent {
